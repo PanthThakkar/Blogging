50454f9d4637486bb7ad643ac8fc8f39a67d551c
PanthThakkar <30841919+PanthThakkar@users.noreply.github.com>
2020-06-16 18:01:52 -0400

    Changes to files
    Adding a database to the

--- app.py
+++ app.py
@@ -1,27 +1,43 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask import request
+from flask import url_for
+from flask import redirect
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
 
-all_posts = [
-    {
-        'title':'Post 1',
-        'content': 'This is the content of post 1',
-        'author': 'Aaron'
-    },
-    {
-        'title':'Post 2',
-        'content': 'This is the content of post 2'
-    }
-]
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50), nullable = False)
+    content = db.Column(db.Text, nullable = False)
+    author = db.Column(db.String(20), nullable = False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' + str(self.id)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET','POST'])
 def posts():
-    return render_template('posts.html', posts=all_posts)
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts=all_posts)
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name, id):
