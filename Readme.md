7c2d3db2d74736dcf97582aa4a83eaf904c997f0
PanthThakkar <30841919+PanthThakkar@users.noreply.github.com>
2020-06-18 22:43:10 -0400

    Complete project

--- Readme.md
+++ Readme.md
@@ -2,3 +2,7 @@ Created a blogging website.
 
 Tutorial used:
 https://www.youtube.com/watch?v=3mwFC4SHY-Y
+
+To run the program:
+
+python app.py
--- app.py
+++ app.py
@@ -50,9 +50,9 @@ def delete(id):
 def edit(id):
     post = BlogPost.query.get_or_404(id)
     if request.method == 'POST':
-        post.title = request.form('title')
-        post.author = request.form('author')
-        post.content = request.form('content')
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
         db.session.commit()
         return redirect('/posts')
     else:
@@ -65,6 +65,24 @@ def hello(name, id):
 @app.route('/onlyget', methods=['GET', 'POST'])
 def get_req():
     return 'You can only get this webpage.'
+
+@app.route('/posts/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('new_post.html')        
+
+@app.route('/contact')
+def getContact():
+    return render_template('contact.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
