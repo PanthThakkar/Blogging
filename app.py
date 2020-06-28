053ee43b1c10d42896da4e50c73ae8a292de84b2
PanthThakkar <30841919+PanthThakkar@users.noreply.github.com>
2020-06-28 17:59:23 -0400

    Addition of Login Creditionals to the website

--- app.py
+++ app.py
@@ -5,10 +5,33 @@ from datetime import datetime
 from flask import request
 from flask import url_for
 from flask import redirect
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm, form
+from wtforms import StringField, PasswordField, BooleanField, validators
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///loginCred.db'
+app.config['SECRET_KEY'] = 'Thisissupposedtobeasecretekey'
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=10, max=100)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = StringField('password', validators=[InputRequired(), Length(min=10, max=100)])
+
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -83,6 +106,32 @@ def new_post():
 def getContact():
     return render_template('contact.html')
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form = form)
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    form = RegisterForm()
+    if request.method == 'POST':
+        post.username = request.form['username']
+        post.email = request.form['email']
+        post.password = request.form['password']
+
+    if form.validate_on_submit():
+        new_user = User(username= post.username, password = post.password, email = post.email)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>New user has been created!</h1>'
+        # return '<h1>' + form.username.data + ' ' + form.password.data + ' ' + form.email.data + '</h1>'
+
+    return render_template('signup.html', form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
